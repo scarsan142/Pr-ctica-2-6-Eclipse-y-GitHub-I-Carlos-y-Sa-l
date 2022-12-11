d6213c178109e1e07541af364a039ff387b5cc1a
Juan Carlos <mail@dominio.com>
2022-12-11 20:21:43 +0100

    Añadido el método producto.

--- ProjectoEgit.java
+++ ProjectoEgit.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class ProjectoEgit {
+	public static int producto(int a, int b) {
+		int c;
+		c = a * b;
+		return c;
+		}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int numero1, numero2, resultado;
+		System.out.print("Introduce primer número: ");
+		numero1 = sc.nextInt();
+		System.out.print("Introduce segundo número: ");
+		numero2 = sc.nextInt();
 
 	}
 
