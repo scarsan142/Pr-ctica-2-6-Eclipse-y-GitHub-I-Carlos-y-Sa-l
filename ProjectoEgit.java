86f4e6ad737a9a6e533dd6b14bca371824186d16
Juan Carlos <mail@dominio.com>
2022-12-11 20:24:32 +0100

    Añadido al método dividir.

--- ProjectoEgit.java
+++ ProjectoEgit.java
@@ -6,6 +6,12 @@ public class ProjectoEgit {
 		c = a * b;
 		return c;
 		}
+		
+	public static int dividir(int a, int b) {
+		int c;
+		c = a / b;
+		return c;
+		}
 
 
 	public static void main(String[] args) {
